602297840734a981aa865866407d9965e515ebb0
Steve Burkett <steve@burkettnet.com>
2017-08-29 11:57:48 -0700

    base of redirect

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -2,8 +2,11 @@ class RedirectController < ApplicationController
   protect_from_forgery except: :redirect
 
   def redirect
+    url = Base64.decode64(params['url'])
+
     #look up matching rule
     #redirect to url
-    @redirect_url = Base64.decode64(params['url'])
+
+    redirect_to url
   end
 end
